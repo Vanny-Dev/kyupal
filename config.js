b2464848b26991145067bd2ec626648a59044031
Jovanny De Leon <roanlandao05@gmail.com>
2024-11-11 11:47:01 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Confession",
+    introTitle: 'Vanny',
+    introDesc: `The world is so strange
+    But why do you keep taking the wrong path?
+    Lost in my lonely heart
+    Behind the word "love" is affection`,
+    btnIntro: '^^HiHi^^',
+    title: 'Do I have chance if I say I like you?',
+    desc: 'Idk why is it so sudden to say this but I really want to say it to you, so I\'ll do it. You know what I mean? <3',
+    btnYes: 'Yes <33',
+    btnNo: 'Nope :3',
+    question: 'Why did you choose Yes? Do you have something to say?',
+    btnReply: 'Send to you <3',
+    reply: 'If I love, I love, and if I don’t, I still love <33333333',
+    mess: 'Let me know what you think about that <3',
+    messDesc: 'Say it to me right now <33333333',
+    btnAccept: 'Okayyyyy <3',
+    //messLink: 'https://github.com/zukahai/Confess-Crush' //Your message link. Example: https://www.facebook.com/messages/t/100014188333536
+}
